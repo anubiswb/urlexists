b668a76bdad3b68bd4d07fa82cd6f0e2c92cdf41
Anubis Web <77882576+anubiswb@users.noreply.github.com>
2022-03-27 11:04:01 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php error_reporting(0);
+ini_set('display_errors', 0);
+
+function url_exists($url){
+    $resURL = curl_init(); 
+    curl_setopt($resURL, CURLOPT_URL, $url); 
+    curl_setopt($resURL, CURLOPT_BINARYTRANSFER, 1); 
+    curl_setopt($resURL, CURLOPT_HEADERFUNCTION, 'curlHeaderCallback'); 
+    curl_setopt($resURL, CURLOPT_FAILONERROR, 1); 
+    curl_exec ($resURL); 
+    $intReturnCode = curl_getinfo($resURL, CURLINFO_HTTP_CODE); 
+    curl_close ($resURL); 
+    if ($intReturnCode != 200 && $intReturnCode != 302 && $intReturnCode != 304)
+     { return 0; } else return 1;
+}
+
+
+echo url_exists('https://www.anubs-web.com');
+
+
+
+?>
